07844206fee750730d2cf384519c0f999ae2354c
Michael Dinh <tonkoalsce@gmail.com>
2021-03-08 21:09:19 +1100

    need to add the change in game state to screen win

--- proj2_ticktacktoe/ticktack.js
+++ proj2_ticktacktoe/ticktack.js
@@ -1,4 +1,5 @@
 
+/*MODEL SECTION*/
 var gameboard = (() => {
     board =  [[0,0,0],[0,0,0],[0,0,0]]
     function isValidPos(x, y) {
@@ -7,47 +8,60 @@ var gameboard = (() => {
     return { board, isValidPos }
 })()
 
-var gamestate = (() => {
+var beginGame = (player1, player2) => {
+    /*Explanation of the displayscreen 
+        0 = the main screen
+        1 = the game screen
+        2 = the afterwin screen */ 
     displayscreen = 0
+    players = [player1, player2]
     playerturn = 1
+
     function nextTurn() {
-        playerturn = 1 ? 2 : 1;
+        this.playerturn = this.playerturn == 1 ? 2 : 1;
     }
     function updateBoard(x, y) {
         if (gameboard.isValidPos(x, y)) {
-            gameboard.board[x][y] = playerturn
-            gamestate.nextTurn()
+            gameboard.board[x][y] = this.playerturn
+            const didwin = checkWin(gameboard, this.playerturn)
+            if (didwin) {
+                this.displayscreen = 2
+            } else {
+                gamestate.nextTurn()
+            }
             updateScreen()
         }
         else {
             console.log("Position taken")
         }
     }
-    return {playerturn, displayscreen, updateBoard, nextTurn}
-})()
+    return {playerturn, displayscreen, updateBoard, nextTurn, players}
+}
 
 const player = (name, playerNumber) => {
     return {name, playerNumber}
 }
 
 const player1 = player("Elf", 1)
-const player2= player("Alice", -1)
+const player2= player("Alice", 2)
 
-const gameState = {
-    turn: 0
-}
+var gamestate = beginGame(player1, player2)
 
+/*CONTROLLER SECTION*/
 
 function checkWin(gameboard, playerNumber) {
-    board = gameboard.board
+    var board = gameboard.board
     var row = [0,0,0]
     var col = [0,0,0]
     var diag = [0,0]
-    const goal = 3 * playerNumber
+    const goal = 3
 
     for (let i = 0; i < board.length; i++) {
         for (let j = 0; j < board[i].length; j++) {
-            v = board[i][j]
+            if (board[i][j] != playerNumber) {
+                continue
+            }
+            var v = 1
             col[j] += v
             row[i] += v
 
@@ -82,24 +96,6 @@ function clickedBox() {
     console.log("clicked")
 }
 
-function updateScreen() {
-
-    $("#turntext").html(`PLAYER ${gamestate.playerturn} TURN`);
-    for (let i = 0; i < gameboard.board.length; i++) {
-        for (let j = 0; j < gameboard.board[0].length; j++) {
-            const tiktacsquareid = `${i + 1},${j + 1}`
-            const tiktacsquare = document.getElementById(tiktacsquareid).childNodes[1]
-            const markclass = gameboard.board[i][j] == 0 ? null :  gameboard.board[i][j] == 1 ? "p1mark" : "p2mark";
-            console.log(gamestate.playerturn)
-            if (markclass != null) {
-                if (!tiktacsquare.classList.contains(markclass)) {
-                    tiktacsquare.classList.add(markclass)
-                }
-            }
-        }
-        
-    }
-}
 // Get the element and iterate
 function updateCoordinate(element) {
     var parentdiv = element.parentNode;
@@ -119,6 +115,42 @@ gridbutton[i].addEventListener("click", function() {
 });
 console.log(gridbutton[i]);
 }
+
+/*VIEW  SECTION*/
+
+function mainScreenWillAppear() {
+
+}
+
+function gameScreenWillAppear() {
+
+}
+
+function showWinner() {
+    $("#winnertext").html(`${gamestate.players[gamestate.playerturn-1].name} \n Wins!`)
+    $("#winnertext").attr('display', 'block');
+
+}
+
+function updateScreen() {
+    $("#turntext").html(`PLAYER ${gamestate.playerturn} TURN`);
+    for (let i = 0; i < gameboard.board.length; i++) {
+        for (let j = 0; j < gameboard.board[0].length; j++) {
+            const tiktacsquareid = `${i + 1},${j + 1}`
+            const tiktacsquare = document.getElementById(tiktacsquareid).childNodes[1]
+            const markclass = gameboard.board[i][j] == 0 ? null :  gameboard.board[i][j] == 1 ? "p1mark" : "p2mark";
+            if (markclass != null) {
+                if (!tiktacsquare.classList.contains(markclass)) {
+                    tiktacsquare.classList.add(markclass)
+                }
+            }
+        }
+        
+    }
+}
+
+
+console.log("okay")
 
 
 
